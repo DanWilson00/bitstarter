6542bc4d4bf2afc5e670e2a327b060e64368947e
Daniel Wilson <dan.wilson00@gmail.com>
2013-07-15 00:38:37 +0000

    Updated grader.js to read from URL

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+A#!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -33,6 +35,12 @@ var assertFileExists = function(infile) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    return instr;
+};
+
+var assertUrlExists = function(inurl) {
+    var instr = inurl.toString();
+    
     return instr;
 };
 
@@ -55,6 +63,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlUrl = function(urldata, checksfile) {
+    $ = cheerio.load(urldata.toString());
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +84,22 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-url, --url <html_url>', 'URL of index.html', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
+
+if(program.url != URL_DEFAULT){
+
+rest.get(program.url).on('complete', function(data) {
+  var checkJson = checkHtmlUrl(data, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+});
+
+}else{
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
